c8b5b0c6cd2bd8d5dc8091693bcd4cb8b5e002c1
dev-yoonjung <dear__yoon@naver.com>
2022-07-28 13:20:13 +0900

    fix: fix request-mapping

---
+++ src/main/java/com/ecommerce/catalog_service/controller/HealthCheckController.java
@@ -0,0 +1,23 @@
+package com.ecommerce.catalog_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/catalog-service")
+@RestController
+public class HealthCheckController {
+
+    private final Environment environment;
+
+    @GetMapping("/health-check")
+    public String status() {
+        return String.format(
+                "It's working in catalog service on port %s",
+                environment.getProperty("local.server.port"));
+    }
+
+}
